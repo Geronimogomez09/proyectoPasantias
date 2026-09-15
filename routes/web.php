3446b0b8fe2f1caee3d5ee2b95e2c143901211aa
Geronimo Gomez <geronimogomezepet20@gmail.com>
2026-09-15 15:36:12 -0300

    Areglando rutas y contenido de archivos

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/usuarios/index', "App\Http\Controllers\ControladorUsuario@Usuario");
 
+Route::get('/usuarios/registro', "App\Http\Controllers\ControladorUsuario@registro");
+
 Route::get('/producto/principal/create', "App\Http\Controllers\ControladorProducto@create");
 
 Route::get('/producto/principal/{id}/edit', 'App\Http\Controllers\ControladorProducto@edit');
